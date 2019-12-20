e74a61c1a9f71569572506380b49ee93f8f2d5d8
CrysiscCaramel <danilukas@mail.ru>
2019-12-20 20:13:45 +0300

    create begind structur

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./css/bootstrap-grid.min.css";
+import "./css/bootstrap-reboot.min.css";
+import "./scss/main.scss"
---
+++ src/js/postcss.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+module.exports = {
+    plugins: [
+      require("autoprefixer"),
+      require("css-mqpacker"),
+      require("cssnano")({
+          preset: [
+              "default", {
+                discardComments: {
+                    removeAll: true
+                }
+              }
+          ]
+      })
+    ]
+  };
